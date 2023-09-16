48f3503b0e7da26c0ee980e8b9cb86ae5cc77259
DangPhuc <89386752+Dangtrongphuc1902@users.noreply.github.com>
2023-09-16 14:46:35 +0700

    Update src firebase

---
+++ JS/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8qcGH1yxu8BbBdGJB4JLOVJXXrghgAZI",
+  authDomain: "shoes-store-1267e.firebaseapp.com",
+  projectId: "shoes-store-1267e",
+  storageBucket: "shoes-store-1267e.appspot.com",
+  messagingSenderId: "652473557160",
+  appId: "1:652473557160:web:c1e6f4dbb234728e57229d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
